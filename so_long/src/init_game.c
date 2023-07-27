d5e9d161a674eda1ea850ad3ad3f70ea3820d4fd
ily <ilyanamegy7@gmail.com>
2023-07-27 20:15:39 +0200

    starting movement

--- so_long/src/init_game.c
+++ so_long/src/init_game.c
@@ -12,7 +12,11 @@ int key_check(int keycode, t_game *game)
 	if (keycode == XK_Escape)
 		end_game("", game, event_end, NULL);
 	else if (keycode == XK_a || keycode == XK_Left)
-		ft_printf("left !\n");
+	{
+		mlx_put_image_to_window(game->mlx_ptr, game->mlx_win, game->bg_img, game->p_pos.x*50, game->p_pos.y*50);
+		game->p_pos.x -= 1;
+		mlx_put_image_to_window(game->mlx_ptr, game->mlx_win, game->rick_img, game->p_pos.x*50, game->p_pos.y*50);
+	}
 	else if (keycode == XK_d || keycode == XK_Right)
 		ft_printf("right !\n");
 	else if (keycode == XK_w || keycode == XK_Up)
@@ -31,15 +35,15 @@ void init_game(t_game *game, char *map_file)
 		show_table(game->map);
 		init_vars(game);
 		check_map_valid(game, map_file);
-		ft_printf("Completed check-in, we can start now!\nControls :\nArrows : up, left, down, right.\nKeyboard : W,A,S,D.\nESC to quit.\n");
+		// ft_printf("Completed check-in, we can start now!\nControls :\nArrows : up, left, down, right.\nKeyboard : W,A,S,D.\nESC to quit.\n");
 		game->mlx_ptr = mlx_init();
 		if (!game->mlx_ptr)
 			end_game("Falling mlx_init(). Aborting, bye.", game, map_error, NULL);
 		game->mlx_win = mlx_new_window(game->mlx_ptr, game->map_size.x  * 50, game->map_size.y * 50, "Event Parameters");
 		if (!game->mlx_win)
 			end_game("Falling mlx_new_window(). Aborting, bye.", game, map_error, game->mlx_ptr);
-		
 		render_map(game);
+		ft_printf("position rick : x = %d  y = %d\n", game->p_pos.x, game->p_pos.y);
 		mlx_hook(game->mlx_win, KEY_RELEASED, KEY_RELEASED_MASK, key_check, game);
 		mlx_hook(game->mlx_win, ON_DESTROY, NO_EVENT_MASK, exit_event, game);
 		mlx_loop(game->mlx_ptr);
---
+++ so_long/src/move_to.c
@@ -0,0 +1,26 @@
+#include "../inc/so_long.h"
+
+void    check_env()
+{
+    
+}
+
+void    move_left()
+{
+
+}
+
+void    move_right()
+{
+    
+}
+
+void    move_up()
+{
+    
+}
+
+void    move_down()
+{
+    
+}
