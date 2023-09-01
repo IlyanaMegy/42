f92a60284b1110871fe8f99931c73ae087f15e53
ily <ilyanamegy7@gmail.com>
2023-09-01 16:36:05 +0200

    trying something new

---
+++ push_swap/old/convert_intab.c
@@ -0,0 +1,90 @@
+#include "../inc/pushswap.h"
+
+int	nb_nbr(t_ps *ps)
+{
+	int		nb_nbr;
+	char	*s;
+	int		i;
+
+	nb_nbr = 0;
+	i = 0;
+	s = ps->pileA;
+	while (s[i])
+	{
+		if (s[i] && s[i] != '\n')
+		{
+			nb_nbr++;
+			while (s[i] && s[i] != '\n')
+				i++;
+		}
+		i++;
+	}
+	return (nb_nbr);
+}
+
+void	print_tab(int *tab)
+{
+	ft_printf("tab\n");
+	int i;
+
+	i = 0;
+	while (tab[i])
+	{
+		ft_printf("%d : %d\n", i, tab[i]);
+		i++;
+	}
+	ft_printf("\n");
+}
+
+int	get_nb(char **tmp, int i, char *pX)
+{
+	int	j;
+
+	j = 0;
+	while (pX[i] && pX[i] != '\n')
+	{
+		j++;
+		i++;
+	}
+	*tmp = malloc(sizeof(char) * (j));
+	if (!(*tmp))
+		return (-1);
+	i -= j;
+	j = 0;
+	while (pX[i] && pX[i] != '\n')
+	{
+		(*tmp)[j] = pX[i];
+		i++;
+		j++;
+	}
+	(*tmp)[j] = '\0';
+	return (i);
+}
+
+int	*int_tab(char *pX, t_ps *ps, int *nb_nb)
+{
+	int *int_pX;
+	int i;
+	char *tmp;
+	int j;
+
+	*nb_nb = nb_nbr(ps);
+	int_pX = malloc(sizeof(int) * (nb_nbr(ps) + 1));
+	int_pX[0] = 0;
+	if (!int_pX)
+		end_prog();
+	i = 0;
+	j = 0;
+	while (pX[i])
+	{
+		i = get_nb(&tmp, i, pX);
+		if (i < 0)
+			end_prog();
+		int_pX[j] = ft_atoi(tmp);
+		j++;
+		i++;
+		free(tmp);
+	}
+	// print_tab(int_pX);
+	return (int_pX);
+}
---
+++ push_swap/old/verifs.c
@@ -0,0 +1,111 @@
+#include "../inc/pushswap.h"
+
+int	len_incr(size_t *start, size_t *end, char *s, int *len_pA)
+{
+	char	*tmp;
+
+	while (*end < ft_strlen(s))
+	{
+		while (s[*start] == ' ')
+			(*start)++;
+		*end = *start;
+		while (s[*end] != ' ')
+			(*end)++;
+		tmp = ft_substr(s, *start, *end - *start);
+		if (!ft_strlen(tmp))
+			break ;
+		else if (!ft_atoi(tmp))
+			if (!(ft_strlen(tmp) == 1 && tmp[0] == '0'))
+				return (1);
+		*len_pA += ft_strlen(tmp) + 1;
+		*start = *end;
+	}
+	return (0);
+}
+
+int	check_args(char **av)
+{
+	int		i;
+	size_t	start;
+	size_t	end;
+	int		len_pileA;
+	int		err;
+
+	i = 1;
+	len_pileA = 0;
+	err = 0;
+	while (av[i])
+	{
+		start = 0;
+		end = 0;
+		err = len_incr(&start, &end, av[i], &len_pileA);
+		if (err)
+			return (0);
+		i++;
+		len_pileA--;
+	}
+	return (len_pileA);
+}
+
+char	*get_number(size_t *start, size_t *end, char *s)
+{
+	char	*tmp;
+
+	while (s[*start] == ' ')
+		(*start)++;
+	*end = *start;
+	while (s[*end] != ' ')
+		(*end)++;
+	tmp = ft_substr(s, *start, *end - *start);
+	if (!ft_strlen(tmp))
+		return (NULL);
+	else if (!ft_atoi(tmp))
+		if (!(ft_strlen(tmp) == 1 && tmp[0] == '0'))
+			return (NULL);
+	*start = *end;
+	return (tmp);
+}
+
+void	join_me(char **pA, char *curr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while ((*pA)[i])
+		i++;
+	while (curr[j])
+	{
+		(*pA)[i + j] = curr[j];
+		j++;
+	}
+	(*pA)[i + j] = '\n';
+	(*pA)[i + j + 1] = 0;
+}
+
+int	fill_pA(char **av, char **pA)
+{
+	int i;
+	size_t start;
+	size_t end;
+	char *res;
+
+	i = 1;
+	while (av[i])
+	{
+		start = 0;
+		end = 0;
+		while (end < ft_strlen(av[i]))
+		{
+			res = get_number(&start, &end, av[i]);
+			if (res)
+				join_me(pA, res);
+			if (!(*pA))
+				return (1);
+		}
+		i++;
+	}
+	(*pA)[ft_strlen(*pA) - 1] = '\0';
+	return (0);
+}
