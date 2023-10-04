bef0f1f8eb2a283489be1593a05204e13d0c0504
Ilyana Megy <ilyanamegy7@gmail.com>
2023-10-04 16:22:50 +0200

    leaks for doublons fixed

--- push_swap/src/init_stack_a.c
+++ push_swap/src/init_stack_a.c
@@ -47,7 +47,10 @@ void	init_stack_a(int argc, char **argv, t_ps **stack_a)
 		i++;
 		number = (int)ft_atoi(argv[i]);
 		if (is_repeated(*stack_a, number))
+		{
+			ft_pslstclear(stack_a);
 			end_prog("Error\n", 1);
+		}
 		else
 			ft_pslstadd_back(stack_a, ft_pslstnew(number));
 	}
