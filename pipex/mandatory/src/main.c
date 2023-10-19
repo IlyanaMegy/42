83ab166d2afc92c6af77e1f77e2acccffca24455
Ilyana Megy <ilyanamegy7@gmail.com>
2023-10-19 16:35:13 +0200

    tester

--- pipex/mandatory/src/main.c
+++ pipex/mandatory/src/main.c
@@ -14,23 +14,23 @@
 
 void	exec(char *cmd, char **env)
 {
-	char	**s_cmd;
+	char	**cmd;
 	char	*path;
 
-	s_cmd = ft_split(cmd, ' ');
-	path = get_path(s_cmd[0], env);
-	if (execve(path, s_cmd, env) == -1)
+	cmd = ft_split(cmd, ' ');
+	path = get_path(cmd[0], env);
+	if (execve(path, cmd, env) == -1)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
-		ft_putendl_fd(s_cmd[0], 2);
-		ft_free_tab(s_cmd);
+		ft_putendl_fd(cmd[0], 2);
+		ft_free_tab(cmd);
 		exit(0);
 	}
 }
 
 void	child(char **av, int *p_fd, char **env)
 {
-	int		fd;
+	int	fd;
 
 	fd = open_file(av[1], 0);
 	dup2(fd, 0);
@@ -41,7 +41,7 @@ void	child(char **av, int *p_fd, char **env)
 
 void	parent(char **av, int *p_fd, char **env)
 {
-	int		fd;
+	int	fd;
 
 	fd = open_file(av[4], 1);
 	dup2(fd, 1);
