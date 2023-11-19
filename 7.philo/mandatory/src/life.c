77ec83dc70061068deb468f5c4c0ebe6074955df
Ilyana Megy <ilyanamegy7@gmail.com>
2023-11-19 14:45:37 +0100

    :construction: developping life

--- 7.philo/mandatory/src/life.c
+++ 7.philo/mandatory/src/life.c
@@ -30,3 +30,34 @@ int	philo_words(t_main *main, int id, char *color, char *s)
 	pthread_mutex_unlock(&main->write);
 	return (0);
 }
+
+void	*life(void *arg)
+{
+	t_main	*main;
+	int		i;
+
+	main = (t_main *)arg;
+	i = main->n_thread;
+	if (main->input.nb_of_times_eat > 0)
+	{
+		while (main->input.nb_of_times_eat > main->philo[i].nb_of_times_ate
+			&& main->philo_dead == 0)
+			do_life(main, i);
+	}
+	else
+	{
+		while (main->philo_dead == 0)
+			if (do_life(main, i))
+				break ;
+	}
+	return (0);
+}
+
+int	do_life(t_main *main, int i)
+{
+	
+}
+
+void	*check_it(void *arg)
+{
+}
