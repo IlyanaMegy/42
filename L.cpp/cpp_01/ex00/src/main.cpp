c78c60b10462e2a71b0b02c8d73a602210451935
Ilyana Megy <ilyanamegy7@gmail.com>
2024-03-08 17:12:38 +0100

    :heavy_plus_sign: added ex00 cpp01

---
+++ L.cpp/cpp_01/ex00/src/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilymegy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 16:40:11 by ilymegy           #+#    #+#             */
+/*   Updated: 2024/03/08 16:40:12 by ilymegy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Zombie.hpp"
+
+int main(void)
+{
+	Zombie *zombie = newZombie("normal zombie");
+	randomChump("random zombie");
+	delete zombie;
+	return (EXIT_SUCCESS);
+}
