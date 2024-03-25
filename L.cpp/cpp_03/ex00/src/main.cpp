5a9ea6dca93e05856a94769203cac8cc51ebf527
Ilyana Megy <ilyanamegy7@gmail.com>
2024-03-25 18:07:51 +0100

    :heavy_plus_sign: added ex00 cpp03

--- L.cpp/cpp_03/ex00/src/main.cpp
+++ L.cpp/cpp_03/ex00/src/main.cpp
@@ -15,18 +15,18 @@
 int	main(void)
 {
 	ClapTrap a("Main");
-	ClapTrap b("Cody");
-	a.attack("some other robot");
+	ClapTrap b("Second");
+	a.attack("some robot");
 	a.takeDamage(1);
 	a.takeDamage(10);
 	a.beRepaired(5);
 	a.showStats();
 	b.showStats();
-	a.attack("some other other robot");
+	a.attack("some other robot");
 	b.beRepaired(3);
 	b.showStats();
 	for (int i = 0; i < 12; i++)
-		b.attack("Cody-clone");
+		b.attack("Second-clone");
 	b.beRepaired(3);
 	return (0);
 }
