22517c5179765f7f044db3fd66217609c453d982
Ilyana Megy <ilyanamegy7@gmail.com>
2024-03-25 18:24:41 +0100

    :heavy_plus_sign: added ex01 cpp03

---
+++ L.cpp/cpp_03/ex01/src/ClapTrap.cpp
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilymegy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 18:10:43 by ilymegy           #+#    #+#             */
+/*   Updated: 2024/03/25 18:10:45 by ilymegy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name): _name(name), _health(100), _energy(50), _attackDamage(20)
+{
+	std::cout << YELLOW << "ClapTrap " << this->_name << " appeared." << RESET << std::endl;
+	return ;
+}
+ClapTrap::~ClapTrap()
+{
+	std::cout << YELLOW << "ClapTrap " << this->_name << " disappeared." << RESET << std::endl;
+	return ;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &copy)
+{
+	*this = copy;
+	std::cout << "ClapTrap copy constructor called" << std::endl;
+	return ;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &assign)
+{
+	if (this != &assign)
+		this->_name = assign._name;
+	return (*this);
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (!_energy || !_health)
+	{
+		if (!_energy )
+			std::cout << RED << "ClapTrap " << _name << " is out of energy!" << RESET << std::endl;
+		if (!_health )
+			std::cout << RED << "ClapTrap " << _name << " health is too low!" << RESET << std::endl;
+		return ;
+	}
+	std::cout << CYAN << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage !" << RESET << std::endl;
+	_energy -= 1;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (!_health)
+	{
+		std::cout << MAGENTA << "ClapTrap " << _name << " is already dead." << RESET << std::endl;
+		return;
+	}
+	if (amount >= _health)
+	{
+		std::cout << RED << "ClapTrap " << _name << " has been beaten to death." << RESET << std::endl;
+		_health = 0;
+		return;
+	}
+	std::cout << BLUE << "ClapTrap " << _name << " takes " << amount << " damage!" << RESET << std::endl;
+	_health -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (!_energy)
+	{
+		std::cout << RED << "ClapTrap " << _name << " is out of energy!" << RESET << std::endl;
+		return ;
+	}
+	std::cout << GREEN << "ClapTrap " << _name << " heals itself for " << amount << " hit points!" << RESET << std::endl;
+	_health += amount;
+	_energy -= 1;
+}
+
+void ClapTrap::showStats(void)
+{
+	std::cout << "-----------------------\n\tStats\n-----------------------" << std::endl;
+	std::cout << "name \t:\t" << _name << std::endl;
+	std::cout << "health \t:\t" << _health << std::endl;
+	std::cout << "energy \t:\t" << _energy << std::endl;
+	std::cout << "attack \t:\t" << _attackDamage << std::endl;
+	std::cout << std::endl;
+}
---
+++ L.cpp/cpp_03/ex01/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilymegy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 18:10:31 by ilymegy           #+#    #+#             */
+/*   Updated: 2024/03/25 18:10:33 by ilymegy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap a("Main");
+	ClapTrap b("Second");
+	a.attack("some robot");
+	a.takeDamage(1);
+	a.takeDamage(10);
+	a.beRepaired(5);
+	a.showStats();
+	b.showStats();
+	a.attack("some other robot");
+	b.beRepaired(3);
+	b.showStats();
+	for (int i = 0; i < 12; i++)
+		b.attack("Second-clone");
+	b.beRepaired(3);
+	return (0);
+}
