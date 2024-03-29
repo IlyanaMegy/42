d156a087cb5001c1eb32294f49f0aebd796210a1
Ilyana Megy <ilyanamegy7@gmail.com>
2024-03-29 14:31:37 +0100

    :construction: work in progress ex02 cpp03

--- L.cpp/cpp_03/ex02/src/FragTrap.cpp
+++ L.cpp/cpp_03/ex02/src/FragTrap.cpp
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/FragTrap.hpp"
 
+FragTrap::FragTrap(const std::string name): ClapTrap(name)
+{
+	std::cout << YELLOW << "FragTrap " << this->_name << " appeared." << RESET << std::endl;
+	this->showStats();
+	std::cout << YELLOW << "||                   ||" << std::endl;
+	std::cout << "\\/                   \\/" << RESET << std::endl;
+	this->_health = 100;
+	this->_energy = 100;
+	this->_attackDamage = 30;
+	this->showStats();
+	std::cout << "\n\n" << std::endl;
+}
+
+FragTrap::FragTrap(FragTrap const &copy): ClapTrap(copy)
+{
+	std::cout << YELLOW << "FragTrap copy constructor called" << RESET << std::endl;
+}
+
+FragTrap &FragTrap::operator=(FragTrap const &assign)
+{
+	std::cout << YELLOW << "FragTrap Assignation operator called" << RESET << std::endl;
+	if (this != assign)
+	{
+		this->_name = assign._name;
+		this->_health = assign._health;
+		this->_energy = assign._energy;
+		this->_attackDamage = assign._attackDamage;
+	}
+	return (*this);
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << YELLOW << "FragTrap " << this->_name << " disappeared." << RESET << std::endl;
+}
