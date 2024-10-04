c14487c3042124da437ffad46c75e14faac525a5
Ilyana Megy <ilyanamegy7@gmail.com>
2024-10-04 21:13:15 +0200

    :heavy_plus_sign: starting AForm.hpp ex02

--- L.cpp/cpp_05/ex02/inc/AForm.hpp
+++ L.cpp/cpp_05/ex02/inc/AForm.hpp
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AFORM_HPP
+# define AFORM_HPP
+# include "Bureaucrat.hpp"
 
+class AForm
+{
+  private:
+	const std::string _name;
+	bool _signed;
+	const int _sign_grade;
+	const int _exec_grade;
+
+  public:
+	// Constructors
+	AForm(void);
+	AForm(AForm const &copy);
+	AForm(int sign_grade, int exec_grade);
+	AForm(const std::string name);
+	AForm(const std::string name, int sign_grade, int exec_grade);
+
+	// Overloaded Operators
+	AForm &operator=(AForm const &assign);
+
+	// Deconstructor
+	virtual ~AForm(void);
+};
+
+#endif
