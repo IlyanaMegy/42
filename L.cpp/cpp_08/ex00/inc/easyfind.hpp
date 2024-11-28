fabcc5af7e02f2de154128d779abb8daf4ab38fe
Ilyana Megy <ilyanamegy7@gmail.com>
2024-11-28 15:28:11 +0100

    :white_check_mark: finished ex00

--- L.cpp/cpp_08/ex00/inc/easyfind.hpp
+++ L.cpp/cpp_08/ex00/inc/easyfind.hpp
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <algorithm>
+# include <exception>
 
+class NotFoundException : public std::exception
+{
+  public:
+	virtual const char *what() const throw()
+	{
+		return ("Number not found");
+	}
+};
+
+template <typename T>
+typename T::iterator easyfind(T &tab, int nb)
+{
+	typename T::iterator i = std::find(tab.begin(), tab.end(), nb);
+	if (i == tab.end())
+		throw NotFoundException();
+	return (i);
+}
+#endif
