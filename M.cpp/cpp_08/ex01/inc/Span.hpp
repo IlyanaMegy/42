8275f75650582012440a9f6f123be028080ae683
Ilyana Megy <ilyanamegy7@gmail.com>
2024-12-05 19:49:25 +0100

    :white_check_mark: finished ex01

--- M.cpp/cpp_08/ex01/inc/Span.hpp
+++ M.cpp/cpp_08/ex01/inc/Span.hpp
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+# define SPAN_HPP
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <exception>
+
+class Span
+{
+private:
+	unsigned int _n;
+	std::vector<int> _v;
+public:
+	Span();
+	Span(unsigned int n);
+	Span(Span const &src);
+	Span &operator=(Span const &src);
+	~Span();
+
+	void addNumber(int n);
+	int shortestSpan();
+	int longestSpan();
+	void addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+
+	class FullContainerException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+
+	class NotEnoughNumbersException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+};
+#endif
--- M.cpp/cpp_08/ex01/src/main.cpp
+++ M.cpp/cpp_08/ex01/src/main.cpp
@@ -10,4 +10,78 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
+#include "../inc/Span.hpp"
+#include "../inc/style.hpp"
 
+int main()
+{
+	std::cout << PINK << "Test vector container\n"<< RESET;
+	Span sp = Span(5);
+	sp.addNumber(5);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	try
+	{
+		sp.addNumber(11);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << REDD << e.what() << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << "shortest span: " << sp.shortestSpan() << std::endl;
+		std::cout << "longest span: " << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << REDD << e.what() << RESET << std::endl;
+	}
+
+
+	std::cout << PINK << "\nTest vector range\n"<< RESET;
+	Span sp2 = Span(100);
+	std::vector<int> v;
+	std::srand(time(NULL));
+	for (int i = 0; i < 100; i++)
+	{
+		
+		int random_int = rand() % 1000000;
+		v.push_back(random_int);
+	}
+	try
+	{
+		sp2.addNumber(v.begin(), v.end());
+		std::cout << "shortest span: " << sp2.shortestSpan() << std::endl;
+		std::cout << "longest span: " << sp2.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << REDD << REDD << e.what() << RESET << std::endl;
+	}
+
+
+	std::cout << PINK << "\nTest vector large range\n"<< RESET;
+	Span sp3 = Span(100000);
+	std::vector<int> v2;
+	std::srand(time(NULL));
+	for (int i = 0; i < 100000; i++)
+	{
+		
+		int random_int = rand() % 1000000000;
+		v2.push_back(random_int);
+	}
+	try
+	{
+		sp3.addNumber(v2.begin(), v2.end());
+		std::cout << "shortest span: " << sp3.shortestSpan() << std::endl;
+		std::cout << "longest span: " << sp3.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << REDD << e.what() << RESET << std::endl;
+	}
+}
