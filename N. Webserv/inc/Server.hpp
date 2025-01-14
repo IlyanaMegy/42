f490934141e203a186b597eacc92ccba111c2f30
Ilyana MEGY <ilymegy@bess-f3r3s2.clusters.42paris.fr>
2025-01-14 18:29:09 +0100

    :heavy_plus_sign: added exception

--- N. Webserv/inc/Server.hpp
+++ N. Webserv/inc/Server.hpp
@@ -7,16 +7,20 @@
 
 class Server
 {
-    private:
-        int _port;
-        struct sockaddr_in _sockAddr;
-        // int _socket;
+private:
+    int _port;
+    struct sockaddr_in _sockAddr;
+    // int _socket;
+public:
+    Server();
+    ~Server();
+    int _socket;
+    class SocketCreationErrException : public std::exception
+    {
     public:
-        Server();
-        ~Server();
-        int _socket;
+        virtual const char *what() const throw();
+    };
 };
-
 
 // Sockets
 // HTTP request
