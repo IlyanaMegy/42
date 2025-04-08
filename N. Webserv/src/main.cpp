2f67e43309ddf2ee40387c40a5a0ae8a262ccf3b
ily <ilyanamegy7@gmail.com>
2025-04-08 17:00:03 +0200

    :seedling: final webserv validated 125/100

--- N. Webserv/src/main.cpp
+++ N. Webserv/src/main.cpp
@@ -1,77 +1,75 @@
-#include "../inc/Epoll.hpp"
-#include "../inc/Server.hpp"
+#include "Signal.hpp"
+#include "Epoll.hpp"
+#include "ServerMonitor.hpp"
+#include "CGI.hpp"
 
-#define PORT 8080
-
-#define FILE "configs/default.conf"
-
-void	runServer(void)
+void	runServer(std::string configFile)
 {
+	ServerMonitor 	serverMonitor(configFile);
+	Epoll			epoll(serverMonitor.getServers());
 
-	Server	server(PORT);
-	Epoll	epoll(server.getSocket().getFd());
+	while (!Signal::shouldStopServer) {
 
-	while (true) {
-		try {
-			epoll.wait();
-		} catch (std::exception &e) {
-			// epoll_ctl
-			std::cout << e.what() << std::endl;
+		epoll.wait();
+
+		for (int i = 0; i < epoll.getTimeoutFdsNb(); i++) {
+			for (std::map<int, Server*>::iterator it = serverMonitor.getServers().begin(); it != serverMonitor.getServers().end(); it++)
+				if (it->second->isClientKnown(epoll.getTimeoutFd(i))) {
+					if (!it->second->getClient(epoll.getTimeoutFd(i))->getRequest())
+						it->second->getClient(epoll.getTimeoutFd(i))->createNewRequest("", &epoll);
+					it->second->getClient(epoll.getTimeoutFd(i))->getRequest()->getResponse().fillError("408", "Request Timeout");
+				}
+				else if (Request* request = it->second->findCGIRequest(epoll.getTimeoutFd(i))) {
+					request->treatCGI();
+					epoll.deleteFd(epoll.getTimeoutFd(i));
+				}
 		}
 
-		// Parcourir les événements
-		for (int i = 0; i < epoll.getReadyFd(); i++) {
-			// CASE SERVER
-			if (epoll.getFd(i) == server.getSocket().getFd()) {
-				try {
-					server.acceptClient();
-				} catch (std::exception &e) {
-					// epoll_ctl
-					std::cout << e.what() << std::endl;
+		for (int i = 0; i < epoll.getReadyFdsNb(); i++) {
+			for (std::map<int, Server*>::iterator it = serverMonitor.getServers().begin(); it != serverMonitor.getServers().end(); it++) {
+				if (epoll.getReadyFd(i) == it->first)
+					it->second->acceptClient(epoll);
+				else if (it->second->isClientKnown(epoll.getReadyFd(i))) {
+					if (epoll.getEvent(i) == (EPOLLIN | EPOLLOUT)
+					|| epoll.getEvent(i) == EPOLLIN)
+						it->second->readFrom(epoll.getReadyFd(i), &epoll);
+					if (epoll.getEvent(i) == (EPOLLIN | EPOLLOUT)
+					|| epoll.getEvent(i) == EPOLLOUT)
+						it->second->sendTo(epoll.getReadyFd(i));
+
+					if (it->second->getClient(epoll.getReadyFd(i))->getShouldClose()) {
+						epoll.deleteFd(epoll.getReadyFd(i));
+						it->second->closeConnection(epoll.getReadyFd(i));
+					}
 				}
-			}
-			else {
-				server.readFrom(i);
-				// Événement sur un socket client
-				// char buffer[BUFFER_SIZE];
-				// ssize_t bytes_read = recv(events[i].data.fd, buffer,
-				// BUFFER_SIZE, 0); if (bytes_read <= 0) {
-				// Connexion fermée ou erreur
-				//   if (bytes_read == 0) {
-				//     std::cout << "Client disconnected." << std::endl;
-				//   } else {
-				//     perror("recv");
-				//   }
-				// }
-				continue;
-
-				// else {
-
-				// ---------------------------------------------------------------------------------------------
-				// ---------------------------------------------------------------------------------------------
-				// HTTP Request saved in buffer var
-				// Traiter la requête et envoyer une réponse
-				// buffer[bytes_read] = '\0';
-				// std::cout << "Received: " << buffer << std::endl;
-
-				// HTTP Response
-				// send(events[i].data.fd, response.c_str(), response.size(),
-				// 0); verifs
-				server.sendTo(i);
+				else if (Request* request = it->second->findCGIRequest(epoll.getReadyFd(i))) {
+					if (epoll.getReadyFd(i) == request->getCGI()->getReadFd())
+						it->second->readFrom(epoll.getReadyFd(i), &epoll, request);
+					else
+						it->second->sendTo(epoll.getReadyFd(i), &epoll, request);
+				}
 			}
 		}
 	}
 }
 
-
-
-int	main(void)
+int	main(int ac, char** av)
 {
-	try {
-		runServer();
+	std::string configFile;
+	
+	if (ac != 1 && ac != 2)
+		return (std::cout << "Error: wrong number of arguments." << std::endl, 1);
+	configFile = (ac == 1) ? "config/webserv.conf" : av[1];
+	try
+	{
+		Signal::setHandler();
+		runServer(configFile);
 	}
-	catch (std::exception &e) {
-    	std::cout << e.what() << std::endl;
+	catch (std::exception &e)
+	{
+		if (Signal::shouldStopServer)
+			return 0;
+		std::cout << e.what() << std::endl;
 		return 1;
 	}
 	return 0;
