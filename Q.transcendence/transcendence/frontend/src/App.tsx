99e3711312d175789ab4b5c929314fcec6ed780c
Ilyana Megy <ilyanamegy7@gmail.com>
2025-08-25 23:12:29 +0200

    :fire: added background

--- Q.transcendence/transcendence/frontend/src/App.tsx
+++ Q.transcendence/transcendence/frontend/src/App.tsx
@@ -1,18 +1,30 @@
 import { Box, Container } from "@chakra-ui/react"
 import { Routes, Route } from "react-router-dom"
-// import Nav from "./components/Nav"
-
+import { AuthProvider } from "./context/Auth"
+import background from "./assets/background.jpg"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Game from "./pages/Game"
 import User from "./pages/User"
+import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
-    <Box minH="100vh" bg="gray.100">
-      {/* <Nav /> */}
-      <Container maxW="container.lg" py={8}>
+    <AuthProvider>
+    <Box 
+      minH="100vh"
+      sx={{
+        bgImage: `url(${background})`,
+        bgPosition: 'center',
+        bgRepeat: 'no-repeat',
+        bgSize: 'cover',
+        position: 'relative',
+      }}
+      position="relative"
+    >
+      <Sidebar />
+      <Container maxW="70%" py={8}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
@@ -22,6 +34,7 @@ function App() {
         </Routes>
       </Container>
     </Box>
+    </AuthProvider>
   )
 }
 
